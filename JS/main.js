568183b1a5b845c6ef2ca94ee83602e3f6789d8f
Krylov <yuri.krylov@gmail.com>
2021-04-17 17:40:05 +0300

    added files

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,29 @@
 const jsonElement = document.getElementById('data');
-const myObject = JSON.parse(jsonElement.textContent);
+const data = JSON.parse(jsonElement.textContent);
+
+function getStrings(s,e){
+    return data.season[s].episode[e];
+}
+function getString(value){
+    if (value){
+        return true
+    }
+    if (!value){
+        return false
+    }
+}
+function buildHTML(s,e){
+    let HTML = `` ;
+    let data = getStrings(s,e);
+    data.forEach((value)=>{
+        HTML += getString(value)
+    })
+}
+
+function switchSeason(s){
+    let value = data.season[s].length;
+    let menuStr = ``;
+    value.forEach((i)=>{
+       menuStr += `<div>${i.epiode.number}</div>`;
+    })
+}
